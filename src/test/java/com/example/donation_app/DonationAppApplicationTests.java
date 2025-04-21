5726e43ab5f8356e77ec17e7ccf52d44acc62d6d
kadyy <ahmedkhaled12004@gmail.com>
2025-04-21 09:38:35 +0200

    Initial commit with empty project

---
+++ src/test/java/com/example/donation_app/DonationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.donation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
